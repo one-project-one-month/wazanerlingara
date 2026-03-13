057cc5e6f9357aa21662aa355e3d241a24462fdd
thanthtooaung-coding <thanthtoo1285@gmail.com>
2026-03-13 17:31:03 +0700

    :zap: fix build issue

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,13 @@ import "@/styles/index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./app/routes/router";
 
-createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+	throw new Error("Root element with id 'root' not found");
+}
+
+createRoot(rootElement).render(
 	<StrictMode>
 		<RouterProvider router={router} />
 	</StrictMode>,
